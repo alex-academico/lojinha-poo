b8437cfb1c7736872371ffdea77e39cf5927d5fa
alex-academico <alexmath_@academico.ufs.br>
2025-07-20 17:48:10 -0300

    Create ProdutoPerecivel.java

---
+++ ProdutoPerecivel.java
@@ -0,0 +1,23 @@
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class ProdutoPerecivel extends Produto {
+
+    public enum FormaArmazenamentoPerecivel {
+        PRATELEIRA, REFRIGERADOR, CONGELADOR, FRESCO
+    }
+
+    private LocalDate validade;
+    private FormaArmazenamentoPerecivel formaArmazenamento;
+
+    public ProdutoPerecivel(String codigo, String nome, BigDecimal preco, int estoque,
+                            LocalDate validade, FormaArmazenamentoPerecivel forma) {
+        super(codigo, nome, preco, estoque);
+        this.validade = validade;
+        this.formaArmazenamento = forma;
+    }
+
+    public String getDetalhes() {
+        return "| Perecível: validade até " + validade + ", armazenado em " + formaArmazenamento;
+    }
+}
