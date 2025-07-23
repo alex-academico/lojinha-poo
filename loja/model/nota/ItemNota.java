8e83f4881d13f0cd92e8cf85b831a15d00d2fd51
LaurenVFR <145593229+LaurenVFR@users.noreply.github.com>
2025-07-22 21:41:32 -0300

    Create ItemNota.java

---
+++ loja/model/nota/ItemNota.java
@@ -0,0 +1,32 @@
+package model.nota;
+
+import java.math.BigDecimal;
+import model.produto.Produto;
+
+public class ItemNota {
+    
+    private Produto produto;
+    private int quantidade;
+    public ItemNota(Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+   
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public BigDecimal getSubtotal() {
+        return produto.getPrecoBase().multiply(BigDecimal.valueOf(quantidade));
+    }
+   
+    public String toString() {
+        return produto.getNome() 
+            + " \n-Quantidade: " + quantidade 
+            + " \n-Subtotal: R$ " + String.format("%.2f", getSubtotal());
+    }
+}
