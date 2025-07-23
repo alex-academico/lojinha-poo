f3a92742ad0f63ef25ab3b9d9cd3f517a4d7adff
alex-academico <alexmath_@academico.ufs.br>
2025-07-23 09:57:17 -0300

    Update ProdutoDigital.java

--- loja/model/produto/ProdutoDigital.java
+++ loja/model/produto/ProdutoDigital.java
@@ -2,8 +2,10 @@ package model.produto;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class ProdutoDigital extends Produto {
+
     private String formato;
     private double tamanhoMB;
     private boolean alugado;
@@ -12,21 +14,42 @@ public class ProdutoDigital extends Produto {
 
     public ProdutoDigital(String codigo, String nome, BigDecimal preco, int estoque,
                           String formato, double tamanhoMB, boolean alugado, LocalDate dataCompra) {
+        
         super(codigo, nome, preco, estoque);
         this.formato = formato;
         this.tamanhoMB = tamanhoMB;
         this.alugado = alugado;
-        this.dataCompra = alugado ? dataCompra : null;
-        this.dataExpiracao = alugado ? dataCompra.plusMonths(1) : null;
+        this.dataCompra = dataCompra; 
+        
+        if (this.alugado) {
+            this.dataExpiracao = LocalDate.now().plusMonths(1);
+        } else {
+            this.dataExpiracao = null;
+        }
     }
 
-    public String getDetalhes() {
-        String info = "| Digital: " + formato + ", " + tamanhoMB + "MB, ";
+    public Produto.TipoProduto getTipoProduto() {
+        return Produto.TipoProduto.DIGITAL;
+    }
+    
+    public String toString() {
+        String detalhesPai = super.toString().substring(0, super.toString().length() - 1);
+        
+        StringBuilder detalhes = new StringBuilder();
+        detalhes.append(detalhesPai)
+                .append(", Tipo: ").append(getTipoProduto().getDescricao())
+                .append(", Formato: ").append(formato)
+                .append(", Tamanho: ").append(tamanhoMB).append("MB");
+
         if (alugado) {
-            info += "Alugado, válido de " + dataCompra + " até " + dataExpiracao;
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            detalhes.append(", Status: Alugado")
+                    .append(", Válido até: ").append(dataExpiracao.format(formatter));
         } else {
-            info += "Comprado";
+            detalhes.append(", Status: Comprado");
         }
-        return info;
+
+        detalhes.append("]");
+        return detalhes.toString();
     }
 }
