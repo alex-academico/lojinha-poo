a83e56903ab4ad5df34b86ea4034ac535c9c41b1
alex-academico <alexmath_@academico.ufs.br>
2025-07-23 12:08:20 -0300

    Update Produto.java

--- loja/model/produto/Produto.java
+++ loja/model/produto/Produto.java
@@ -3,22 +3,6 @@ package model.produto;
 import java.math.BigDecimal;
 
 public abstract class Produto {
-    
-    public enum TipoProduto {
-        FISICO("Físico"),
-        DIGITAL("Digital"),
-        PERECIVEL("Perecível");
-
-        private final String descricao;
-
-        TipoProduto(String descricao) {
-            this.descricao = descricao;
-        }
-
-        public String getDescricao() {
-            return descricao;
-        }
-    }
 
     private String codigo;
     private String nome;
@@ -32,18 +16,24 @@ public abstract class Produto {
         this.estoque = estoque;
     }
 
-    public abstract TipoProduto getTipoProduto();
-
     public String getCodigo() {
         return codigo;
     }
 
-    public String getNome() {
-        return nome;
-    }
-    
     public int getEstoque() {
         return estoque;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPrecoBase(BigDecimal precoBase) {
+        this.precoBase = preco;
+    }
+
+    public BigDecimal getPrecoBase() {
+        return precoBase;
     }
 
     public void adicionarEstoque(int quantidade) {
@@ -58,7 +48,11 @@ public abstract class Produto {
         return false;
     }
 
+    public abstract String getTipo();
+    public abstract String getDetalhes();
+
     public String toString() {
         return "Produto [Código:" + codigo + ", Nome:" + nome + ", Preço: R$" + precoBase + ", Estoque:" + estoque + "]";
     }
 }
+
