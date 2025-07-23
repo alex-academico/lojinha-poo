536c5bf7cafab3d79c9d60b2eaa1be9ead00cd9e
Wanessa <159792112+wanessasanttos@users.noreply.github.com>
2025-07-23 10:09:57 -0300

    Create Identificavel.java

---
+++ loja/model/cliente/Identificavel.java
@@ -0,0 +1,4 @@
+// essa definição de interface faz com que seja possível reutilizar e facilite na passagem de outros metodos contanto que mantenham a estrutura
+public interface Identificavel {
+    String getId();
+}
